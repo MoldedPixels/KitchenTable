8187ed84ee9d789c4a4445ceea26b891a65acad8
MoldedPixels <43187186+MoldedPixels@users.noreply.github.com>
2021-05-07 13:31:02 -0400

    Revert "Revert "Spring Booted""

--- Project_2/src/main/java/com/project2/kitchentable/beans/User.java
+++ Project_2/src/main/java/com/project2/kitchentable/beans/User.java
@@ -1,26 +1,107 @@
 package com.project2.kitchentable.beans;
 
-public class User{
-	private int userID;
+import java.io.Serializable;
+import java.util.UUID;
+
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+@Table("user")
+public class User implements Serializable {
+	private static final long serialVersionUID = 9162925644169168189L;
+	@PrimaryKeyColumn(
+			name="id",
+			ordinal=0,
+			type=PrimaryKeyType.PARTITIONED)
+	private UUID userID;
+	@Column
 	private String firstname;
+	@Column
 	private String lastname;
-	private int familyID;
-	private int kitchenID;
-	private int userTypeID;
+	@Column
+	private UUID familyID;
+	@PrimaryKeyColumn(
+			name="kitchenid",
+			ordinal=1,
+			type=PrimaryKeyType.CLUSTERED)
+	private UUID kitchenID;
+	@PrimaryKeyColumn(
+			name="usertype",
+			ordinal=2,
+			type=PrimaryKeyType.CLUSTERED)
+	private int userType;
 	
 	public User() {
 		super();
+	}
+
+	public UUID getUserID() {
+		return userID;
+	}
+
+	public void setUserID(UUID userID) {
+		this.userID = userID;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public UUID getFamilyID() {
+		return familyID;
+	}
+
+	public void setFamilyID(UUID familyID) {
+		this.familyID = familyID;
+	}
+
+	public UUID getKitchenID() {
+		return kitchenID;
+	}
+
+	public void setKitchenID(UUID kitchenID) {
+		this.kitchenID = kitchenID;
+	}
+
+	public int getUserType() {
+		return userType;
+	}
+
+	public void setUserType(int userType) {
+		this.userType = userType;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((familyID == null) ? 0 : familyID.hashCode());
 		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + ((kitchenID == null) ? 0 : kitchenID.hashCode());
 		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
-		result = prime * result + userID;
+		result = prime * result + ((userID == null) ? 0 : userID.hashCode());
+		result = prime * result + userType;
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -30,62 +111,40 @@ public class User{
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
+		if (familyID == null) {
+			if (other.familyID != null)
+				return false;
+		} else if (!familyID.equals(other.familyID))
+			return false;
 		if (firstname == null) {
 			if (other.firstname != null)
 				return false;
 		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (kitchenID == null) {
+			if (other.kitchenID != null)
+				return false;
+		} else if (!kitchenID.equals(other.kitchenID))
 			return false;
 		if (lastname == null) {
 			if (other.lastname != null)
 				return false;
 		} else if (!lastname.equals(other.lastname))
 			return false;
-		if (userID != other.userID)
+		if (userID == null) {
+			if (other.userID != null)
+				return false;
+		} else if (!userID.equals(other.userID))
 			return false;
-		if (userTypeID != other.userTypeID)
+		if (userType != other.userType)
 			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
-		return "Player [name=" + firstname + lastname + ", type" + userTypeID + ", userID=" + userID + "]";
-	}
-	public int getUserID() {
-		return userID;
-	}
-	public void setUserID(int userID) {
-		this.userID = userID;
-	}
-	public int getKitchenID() {
-		return kitchenID;
-	}
-	public void setKitchenID(int kitchenID) {
-		this.kitchenID = kitchenID;
-	}
-	public int getUserTypeID() {
-		return userTypeID;
-	}
-	public void setUserTypeID(int userTypeID) {
-		this.userTypeID = userTypeID;
-	}
-	public String getFirstName() {
-		return firstname;
-	}
-	public void setFirstName(String fname) {
-		this.firstname = fname;
-	}
-	public String getLastName() {
-		return lastname;
-	}
-	public void setLastName(String lname) {
-		this.lastname = lname;
+		return "User [userID=" + userID + ", firstname=" + firstname + ", lastname=" + lastname + ", familyID="
+				+ familyID + ", kitchenID=" + kitchenID + ", userType=" + userType + "]";
 	}
 
-	public int getFamilyID() {
-		return familyID;
-	}
-
-	public void setFamilyID(int familyID) {
-		this.familyID = familyID;
-	}
 }
--- Project_2/src/main/java/com/project2/kitchentable/controllers/UserController.java
+++ Project_2/src/main/java/com/project2/kitchentable/controllers/UserController.java
@@ -1,5 +1,10 @@
 package com.project2.kitchentable.controllers;
 
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -11,14 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.project2.kitchentable.beans.User;
 import com.project2.kitchentable.services.UserService;
+import com.project2.kitchentable.services.UserServiceImpl;
+import com.project2.kitchentable.utils.JWTParser;
 
 @RestController
 public class UserController {
-	private static UserService us;
+	private UserService userService;
+	private JWTParser tokenService;
 	
 	@Autowired
-	public void setUserService(UserService us) {
-		this.us = us;
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@Autowired
+	public void setTokenServicer(JWTParser parser) {
+		this.tokenService = parser;
 	}
 	
 //	@GetMapping(value = "users", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -28,7 +41,7 @@ public class UserController {
 	
 	@PostMapping("users")
 	public ResponseEntity<User> registerUser(@RequestBody User u) {
-		us.addUser(u);
+		userService.addUser(u);
 		return ResponseEntity.status(201).body(u);
 	}
 }
