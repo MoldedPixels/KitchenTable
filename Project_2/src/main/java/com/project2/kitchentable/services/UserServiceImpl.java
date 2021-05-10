d28cd54af080bf18c1209d7ae71c3c7c634beb1a
Christopher Morrison <43414d@gmail.com>
2021-05-09 22:49:55 -0500

    fixed implementation of get user by name, now returns a filtered flux of all users with the name matching fname and lname

--- Project_2/src/main/java/com/project2/kitchentable/services/UserServiceImpl.java
+++ Project_2/src/main/java/com/project2/kitchentable/services/UserServiceImpl.java
@@ -6,41 +6,42 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import com.project2.kitchentable.beans.User;
 import com.project2.kitchentable.data.ReactiveUserRepo;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 @Service
 public class UserServiceImpl implements UserService {
+	@SuppressWarnings("unused")
 	private static Logger log = LogManager.getLogger(UserServiceImpl.class);
 	@Autowired
 	private ReactiveUserRepo userRepo;
-	
+
 	@Override
-	public Mono<User> getUser(String fname, String lname) {
-		// TODO Auto-generated method stub
-		return null;
+	public Flux<User> getUsersByName(String fname, String lname) {
+		return userRepo.findAll().filter((u) -> (u.getFirstname().equals(fname) && u.getLastname().equals(lname)));
 	}
-	
+
 	@Override
 	public Mono<User> addUser(User u) {
 		return userRepo.insert(u);
 	}
-	
+
 	@Override
 	public Mono<User> updateUser(User u) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public Flux<User> getUsers() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public Mono<User> getUserByID(UUID userID) {
 		return userRepo.findById(userID.toString());
