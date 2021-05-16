193ee102e8988d4896cf6845d6c3ab3f5e9dde86
Christopher Morrison <43414d@gmail.com>
2021-05-15 20:18:17 -0500

    smelly code

--- Project_2/src/main/java/com/project2/kitchentable/beans/Recipe.java
+++ Project_2/src/main/java/com/project2/kitchentable/beans/Recipe.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.apache.commons.lang.ArrayUtils;
-import org.springframework.data.cassandra.core.cql.Ordering;
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
@@ -14,15 +13,9 @@ import org.springframework.data.cassandra.core.mapping.Table;
 @Table("recipe")
 public class Recipe implements Serializable {
 	private static final long serialVersionUID = -5825156404073366871L;
-	@PrimaryKeyColumn(
-			name="recipeid",
-			ordinal=0,
-			type=PrimaryKeyType.PARTITIONED)
+	@PrimaryKeyColumn(name = "recipeid", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
 	private UUID recipeId;
-	@PrimaryKeyColumn(
-			name="cuisine",
-			ordinal=1,
-			type=PrimaryKeyType.CLUSTERED)
+	@PrimaryKeyColumn(name = "cuisine", ordinal = 1, type = PrimaryKeyType.CLUSTERED)
 	private int cuisine;
 	@Column
 	private String recipeName;
